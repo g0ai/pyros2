735545cb3ed29cae01ec1a17e642c5e7002665d5
Ibrahim Abdulhafiz <iabdulhafiz@ryerson.ca>
2024-08-07 18:04:47 -0400

    corrected issue remane

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ setup(
     version="1.0.0",
     author="Ibrahim Abdulhafiz",
     author_email="ibrahim@g0ai.com",
-    url="https://github.com/G0ai/ros0",
+    url="https://github.com/G0ai/pyros2",
     description="Minimalistic ros-compatible python bridge.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     license="MIT",
     packages=find_packages(),
     package_dir={
-        'ros0': 'ros0'
+        'pyros2': 'pyros2'
     },
     entry_points={"console_scripts": ["ros0 = ros0.main:main"]},
     classifiers=(
@@ -25,7 +25,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
-    keywords="ros0 g0ai zmq",
+    keywords="pyros2 ros ros2 g0ai zmq",
     install_requires=requirements,
     zip_safe=False,
 )
