1c37b45462baf95c24c4f151c8f1e7cd98b93611
Ibrahim Abdulhafiz <iabdulhafiz@ryerson.ca>
2024-08-14 09:45:16 -0400

    added new theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2024, Ibrahim Abdulhafiz'
 author = 'Ibrahim Abdulhafiz'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1:alpha'
+release = 'v0.1'
 
 
 # -- General configuration ---------------------------------------------------
@@ -47,7 +47,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = "sphinxawesome_theme"
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
